4cae032b4899febe20580ee01249bbf38a53e3b6
Rodrigo Bello <rodrigoabbello@gmail.com>
2018-09-29 23:43:59 -0300

    Add the DraggableIcon component to the UI utils lib

---
+++ src/components/UI/Icons/DraggableIcon/DraggableIcon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './DraggableIcon.css';
+
+const DraggableIcon = () => (
+  <div className="DraggableIcon">
+    <div />
+    <div />
+  </div>
+);
+
+export default DraggableIcon;
